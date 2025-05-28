9a398e2d83d29444562664ae08f7a18543d84912
Padmaunify <laxmi.panda@unifyapps.co.in>
2025-05-28 15:09:00 +0530

    add all changes

--- pwdG/src/App.jsx
+++ pwdG/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length,setLength]=useState(8) // 8 is the default value
+  const [numallow,setNumallow]=useState(false)
+   const [charallow,setCharallow]=useState(false)
+   const [password,setPassword]=useState("")
+
+   const passwordGenerator=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numallow){
+      str+="0123456789"
+    }
+    else{
+      str+="!@#$%^&*-_+=[]{}~`"
+    }
+
+   } ,[length,numallow,charallow,setPassword])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='text-4xl text-center'>Password Generator</h1>
     </>
   )
 }
