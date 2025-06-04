987dc21d3fa66d9115e0130e5aefdc28f8dbf373
Padmaunify <laxmi.panda@unifyapps.co.in>
2025-06-04 10:57:41 +0530

    add all changes

---
+++ next_js_f/src/app/(dashboard)/dashboard/analytics/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+ 
+
+export default function analytics(){
+    return (
+        <h2>Analytics Dashboard</h2>
+    )
+}
---
+++ next_js_f/src/app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+import React from "react"; 
+
+
+const Page=({params}:{params:{id:string}})=>{
+    const {id}=params;
+    return (
+        <div>User Profile: {id}</div> 
+    )
+} 
+
+export default Page
